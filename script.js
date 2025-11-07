d1ae8870ed3e610ed32d1f052ce961e9730dcf7c
Lumi <lumi.f3m@gmail.com>
2025-11-06 22:31:31 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+// Luc1d Icons
+lucide.createIcons();
+const appId = typeof__app_id!=='undefined'?__app_id : 'default-app-id';
+const audioPlayer = document.getElementById('audio-player');
+const mockPlaylist = [
+    {id: 1, title: "Crazy Story", artist: "King Von", album: "Crazy Story", filename: "crazystory"}
+]
